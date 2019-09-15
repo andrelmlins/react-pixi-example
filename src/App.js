22b6b116e24564fe0b852da136e4ddd4ac021ced
André Lins <andre.lins@softplan.com.br>
2019-09-15 10:33:23 -0300

    Create Readme.md

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import { Stage, Container, NineSlicePlane } from "@inlet/react-pixi";
 
 import Soccer from "./Soccer";
 
-const App = () => {
-  return (
-    <Stage
+const App = () => (
+  <Stage
+    width={document.body.clientWidth}
+    height={document.body.clientHeight}
+    options={{ backgroundColor: 0x1d2230 }}
+  >
+    <NineSlicePlane
       width={document.body.clientWidth}
       height={document.body.clientHeight}
-      options={{ backgroundColor: 0x1d2230 }}
-    >
-      <NineSlicePlane
-        width={document.body.clientWidth}
-        height={document.body.clientHeight}
-        x={0}
-        y={0}
-        image="./assets/campo.jpg"
-      />
-      <Container x={0} y={0}>
-        <Soccer />
-      </Container>
-    </Stage>
-  );
-};
+      x={0}
+      y={0}
+      image="./assets/campo.jpg"
+    />
+    <Container x={0} y={0}>
+      <Soccer />
+    </Container>
+  </Stage>
+);
 
 export default App;
